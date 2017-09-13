3c2c43faa2e35aadaba73dd011621b540624cb6f
Younes Elfeitori <youneselfeitori@gmail.com>
2017-09-13 18:39:11 -0400

    Init

---
+++ Hello_World_GUI.py
@@ -0,0 +1,10 @@
+#Created by: Younes Elfeitori
+#Created on: 13Sep 2013
+#Created for: ICS3U
+#Daily Assignment: Unit0-02
+#This program displays Hello World but in GUI format
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
