f689d1da3eff761bcaf4a050eaf1306ab192245b
Younes Elfeitori <youneselfeitori@gmail.com>
2017-09-13 19:08:31 -0400

    Init

---
+++ address_GUI.py
@@ -0,0 +1,9 @@
+#Created by: Younes Elfeitori
+#Created on: Sep 13 2017
+#Created for: ICS3U
+#This program is my address program and, but with a GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
